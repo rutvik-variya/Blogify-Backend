0446e744792f78c39f9ed88b6daab2dcf9272b66
rutvik-variya <rutvik.bvminfotech@gmail.com>
2026-06-18 11:43:46 +0530

    small changes done

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -3,7 +3,7 @@ const validate = require("../middlewares/validate.middleware");
 const verifyToken = require("../middlewares/auth.middleware");
 const { blogSchema, updateBlogSchema } = require("../validators/blog.validator");
 const { createBlog, deleteBlog, updateBlog, getAllBlog, getSingleBlog, toggleLike, toggleBookmark } = require("../controllers/blog.controller")
-
+const { changeBlogStatus } = require("../controllers/admin.controller")
 const upload = require("../middlewares/upload.middleware")
 const router = express.Router();
 
@@ -11,6 +11,8 @@ const router = express.Router();
 router.post("/", verifyToken, upload.single("featuredImage"), validate(blogSchema), createBlog);
 router.delete("/:id", verifyToken, deleteBlog)
 router.put("/:id", verifyToken, upload.single("featuredImage"), validate(updateBlogSchema), updateBlog)
+router.patch("/:id/status", verifyToken, changeBlogStatus)
+
 
 // like and bookmark 
 router.patch("/:id/like", verifyToken, toggleLike)
