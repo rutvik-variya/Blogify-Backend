dfdae3d2a4171948874db5718f3b6496213b3df3
rutvik-variya <rutvik.bvminfotech@gmail.com>
2026-06-19 15:19:14 +0530

    Fix production cors

--- src/app.js
+++ src/app.js
@@ -14,25 +14,16 @@ const publicRoute = require("./routes/public.route")
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    process.env.CLIENT_URL
-];
-
 app.use(
     cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-
-            if (allowedOrigins.includes(origin)) {
-                return callback(null, true);
-            }
-
-            return callback(new Error("CORS blocked"));
-        },
+        origin: [
+            "http://localhost:5173",
+            "https://blogify-frontend-mocha.vercel.app"
+        ],
         credentials: true,
     })
 );
+
 
 app.use("/api/auth/", authRoutes);
 app.use("/api/admin/", adminRoutes);
